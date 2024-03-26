42422399e26840701d1e9871a1d8ba57e9806b75
heitork1 <heitorklein70@gmail.com>
2024-03-26 16:38:27 -0300

    seriePi e primos

--- primos.hs
+++ primos.hs
@@ -1,4 +1,4 @@
 primo m 
-  | m == 1 || m == 2 || m == 3 || m == 5 || m == 7 = True
+  | m == 1 || m == 2 || m == 3 || m == 5 = True
   | m `mod` 2 == 0 || m `mod` 3 == 0 || m `mod` 5 == 0 = False
   | otherwise = True
