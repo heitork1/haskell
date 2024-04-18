7a257bbd1850e0e0a9c9333f6678d0f420f0b623
Heitor Henrique Klein <74987858+heitork1@users.noreply.github.com>
2024-04-18 16:25:25 -0300

    Update lista2.hs

--- lista2.hs
+++ lista2.hs
@@ -1,8 +1,12 @@
+-- só posso utilizar : para adicionar os valores a esquerda
+
 pertence n [] = False
 pertence n (x:xs) | n == x = True
-                  | otherwise = pertence n (xs) 
+                  | otherwise = pertence n xs
 
-intersecao [] _ = [] 
+intersecao [] _ = []
 intersecao _ [] = []
 intersecao (x:xs) (y:ys) | pertence x ys = x : intersecao xs ys
                          | otherwise = intersecao xs ys
+inverso [x] = [x]
+inverso (x:xs) = inverso xs ++ [x]
