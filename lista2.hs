3689a0f08f65d322103dc3a99e41b6c6c2262a2e
Heitor Henrique Klein <74987858+heitork1@users.noreply.github.com>
2024-04-21 19:34:23 -0300

    Update lista2.hs

--- lista2.hs
+++ lista2.hs
@@ -11,12 +11,6 @@ intersecao (x:xs) (y:ys) | pertence x ys = x : intersecao xs ys
 inverso [x] = [x]
 inverso (x:xs) = inverso xs ++ [x]
 
-nUltimos [] = []
-nUltimos n (x:xs) | n >= length x:xs = x:xs
-               | otherwise = nUltimos n (x:xs - last xs)
-
---nUltimos :: Int -> [a] -> [a]
---nUltimos _ [] = []  -- Se a lista de entrada for vazia, retorna uma lista vazia
---nUltimos n xs
-  --  | n >= length xs = xs  -- Se n for maior ou igual ao comprimento de xs, retorna xs inteira
-    -- | otherwise = drop (length xs - n) xs  -- Remove os primeiros length xs - n elementos de xs
+nUltimos _ [] = []
+nUltimos n (x:xs) | n >= length (x:xs) = (x:xs)
+               | otherwise = nUltimos n xs 
